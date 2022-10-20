2a8dead81e61022f05e5ad4959dab1f10b500254
graemebates <graeme.gilmour.bates@gmail.com>
2022-10-20 16:58:12 -0700

    Updated binary tree to not be two class structures, too confusing, instead binary tree class with node struct

--- binaryTree/binarytree.h
+++ binaryTree/binarytree.h
@@ -8,165 +8,96 @@
 #define binartTree_h
 
 template <class T>
-class BinaryNode{
-  public:
-    BinaryNode(T);
-    void destroy_node();
-
-    void add_left(BinaryNode<T>);
-    void add_right(BinaryNode<T>);
-
-    void push(T, std::string);
-
-    void insert(T);
-    T recursive_search(std::string);
-
-    T package;
-    BinaryNode<T> *left;
-    BinaryNode<T> *right;
-};
-
-template <class T>
-BinaryNode<T>::BinaryNode(T val){
-  package=val;
-  left=NULL;
-  right=NULL;
-}
-
-template <class T>
-void BinaryNode<T>::destroy_node(){
-  if(left!=NULL){
-    left->destroy_node();
-  }
-  if(right!=NULL){
-    right->destroy_node();
-  }
-  delete this;
-}
-
-template <class T>
-void BinaryNode<T>::insert(T i_value){
-  if(i_value>package){
-    if(left==NULL){
-      left=new BinaryNode(i_value);
-    } else {
-      // if(i_value<left->package){
-      //   auto temp=new BinaryNode(i_value);
-      //   temp->left=left;
-      //   left=temp;
-      // } else {
-      //   left->insert(i_value);
-      // }
-      left->insert(i_value);
-    }
-  }
-  else{
-    if(right==NULL){
-      right=new BinaryNode(i_value);
-    } else {
-      // if(i_value>right->package){
-      //   auto temp=new BinaryNode(i_value);
-      //   temp->right=right;
-      //   right=temp;
-      // } else {
-      //   right->insert(i_value);
-      // }
-      right->insert(i_value);
-    }
-  }
-}
-
-template <class T>
-void BinaryNode<T>::push(T value, std::string location){
-  if(location.length()>1){
-    if(location[0]=='0'){
-      if(right!=NULL){
-        right->push(value, location.substr(1));
-      } else {
-        //This will just go as far as it can if search is out of bounds
-        right=new BinaryNode(value);
-      }
-    } else {
-      if(left!=NULL){
-        left->push(value, location.substr(1));
-      } else {
-        //This will just go as far as it can if search is out of bounds
-        left=new BinaryNode(value);
-      }
-    }
-  } else {
-    auto temp=new BinaryNode(value);
-    if(location[0]=='0'){
-      temp->right=right;
-      right=temp;
-    } else {
-      temp->left=left;
-      left=temp;
-    }
-  }
-}
-
-template <class T>
-T BinaryNode<T>::recursive_search(std::string binary){
-  if(binary.length()!=0){
-    if(binary[0]=='0'){
-      return right->recursive_search(binary.substr(1));
-    }
-    else{
-      return left->recursive_search(binary.substr(1));
-    }
-  } else {
-    return package;
-  }
-}
-
-
-template <class T>
 class BinaryTree{
   public:
-    BinaryTree(T);
-    ~BinaryTree();
-    void insert(T);
-    void push(T, std::string);
-    void print_tree();
-    T search(std::string);
-    BinaryNode<T> *root;
+    struct bNode{
+      T package;
+      bNode *left;
+      bNode *right;
+    };
+    bNode *root;
+    BinaryTree(){
+      root=NULL;
+    }
+    ~BinaryTree(){
+      bNode *current=root;
+      destroyTree(current);
+    }
+    void print(){
+      print("", root, false);
+    }
+    void print(const std::string &prefix, const bNode *node, bool isLeft){
+      if(node!=NULL){
+        std::cout << prefix;
+        std::cout << (isLeft ? "├──" : "└──");
+        std::cout << node->package << std::endl;
+        print(prefix+(isLeft? "|  " : "   "), node->left, true);
+        print(prefix+(isLeft? "|  " : "   "), node->right, false);
+      }
+    }
+    void insert(T value){
+      root=insert(root, value);
+    }
+    bNode* insert(bNode *node, T value){
+      if(node==NULL){
+        node=new bNode;
+        node->left=NULL;
+        node->right=NULL;
+        node->package=value;
+        return node;
+      } else{
+        if(value < node->package){
+          node->right=insert(node->right, value);
+        } else if(value > node->package){
+          node->left=insert(node->left, value);
+        }
+        return node;
+      }
+    }
+    int height(){
+      return height(root);
+    }
+    int height(bNode *node){
+      if(node==NULL){
+        return 0;
+      }
+      return(std::max(height(node->left), height(node->right)) + 1);
+    }
+    bNode *rotate_left(bNode *node){
+      //take node to the right make it root
+      //with current node on new roots left
+      //with contents of target nodes left on the right of this node
+      bNode *temp=node->right->left;
+      bNode *target=node->right;
+      target->left=node;
+      node->right=temp;
+      return target;
+    }
+    bNode *rotate_right(bNode *node){
+      bNode *temp=node->left->right;
+      bNode *target=node->left;
+      target->right=node;
+      node->left=temp;
+      return target;
+    }
+    bNode *left_right_rotate(bNode *node){
+      node->left=rotate_left(node->left);
+      node=rotate_right(node);
+      return node;
+    }
+    bNode *right_left_rotate(bNode *node){
+      node->right=rotate_right(node->right);
+      node=rotate_left(node);
+      return node;
+    }
   private:
-    void destroy_tree();
+    void destroyTree(bNode *node){
+      if(node!=NULL){
+        destroyTree(node->left);
+        destroyTree(node->right);
+        delete node;
+      }
+    }
 };
-
-template <class T>
-BinaryTree<T>::BinaryTree(T r_value){
-  root=new BinaryNode(r_value);
-}
-
-template <class T>
-void BinaryTree<T>::destroy_tree(){}
-
-template <class T>
-BinaryTree<T>::~BinaryTree(){
-  root->destroy_node();
-}
-
-template <class T>
-void BinaryTree<T>::insert(T i_value){
-  root->insert(i_value);
-}
-
-template <class T>
-void BinaryTree<T>::push(T value, std::string location){
-  root->push(value, location);
-}
-
-template <class T>
-T BinaryTree<T>::search(std::string search){
-  return root->recursive_search(search);
-}
-
-template <class T>
-void BinaryTree<T>::print_tree(){
-  int indentation=0;
-
-}
 
 #endif
--- binaryTree/main.cpp
+++ binaryTree/main.cpp
@@ -2,139 +2,131 @@
 #include "../unitTest/test.h"
 #include <iostream>
 
-void test_simple_insert_value(test_result &r){
-  auto test_tree=BinaryTree(5);
-  test_tree.insert(2);
-  test_tree.insert(3);
-  try{
-    if(test_tree.root->right->left->package==3){
-      //woo leaf expected;
+void insert_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(5);
+  tree.insert(14);
+  if(tree.root->package==10){
+    if(tree.root->right->package==5 && tree.root->left->package==14){
       r.passed=true;
-    } else {
+    } else{
       r.passed=false;
-      r.log="Expected 2, Got: "+std::to_string(
-        test_tree.root->right->right->package
-      );
+      r.log="Leafs not correct";
     }
-  }
-  catch(std::exception &e){
+  } else{
     r.passed=false;
-    r.log="Error: ";
+    r.log="Root not correct";
   }
 }
 
-void test_insert_greater(test_result &r){
-  auto test_tree=BinaryTree(5);
-  test_tree.insert(6);
-  if(test_tree.root->left->package==6){
-    r.passed=true;
-  } else {
+void height_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(5);
+  if(tree.height()!=2){
     r.passed=false;
-    r.log="something went wrong";
+    return;
   }
+  tree.insert(14);
+  if(tree.height()!=2){
+    r.passed=false;
+    return;
+  }
+  tree.insert(15);
+  if(tree.height()!=3){
+    r.passed=false;
+    return;
+  }
+  r.passed=true;
 }
 
-void test_insert_lesser(test_result &r){
-  auto test_tree=BinaryTree(5);
-  test_tree.insert(2);
-  if(test_tree.root->right->package==2){
-    r.passed=true;
-  } else {
-    r.passed=false;
-    r.log="something went wrong";
-  }
-}
-
-void test_insert_many(test_result &r){
-  auto test_tree=BinaryTree(5);
-  int i=0;
-  while(i<11){
-    if(i!=5){
-      test_tree.insert(i);
-    }
-    i++;
-  }
-  if(test_tree.root->left->package==6){
-    if(test_tree.root->right->package==0){
+void rotate_left_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(5);
+  tree.insert(4);
+  tree.root=tree.rotate_left(tree.root);
+  if(tree.root->package==5){
+    if(tree.root->left->package==10){
       r.passed=true;
-    } else {
+    } else{
       r.passed=false;
-      r.log="Expected to get 0 on the first right Got "+std::to_string(
-        test_tree.root->right->package
-      );
     }
-  } else {
+  } else{
     r.passed=false;
-    r.log="Expected to get 6 on the first left";
+  }
+}
+
+void rotate_right_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(15);
+  tree.insert(20);
+  tree.root=tree.rotate_right(tree.root);
+  if(tree.root->package==15){
+    if(tree.root->left->package==20){
+      r.passed=true;
+    } else{
+      r.passed=false;
+    }
+  } else{
+    r.passed=false;
+  }
+}
+
+void left_right_rotate_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(15);
+  tree.insert(12);
+  tree.root=tree.left_right_rotate(tree.root);
+  r.passed=false;
+  if(tree.root->package==12){
+    if(tree.root->right->package==10){
+      if(tree.root->left->package==15){
+        r.passed=true;
+      }
+    }
+  }
+}
+
+void right_left_rotate_test(test_result &r){
+  auto tree=BinaryTree<int>();
+  tree.insert(10);
+  tree.insert(3);
+  tree.insert(5);
+  tree.root=tree.right_left_rotate(tree.root);
+  r.passed=false;
+  if(tree.root->package==5){
+    if(tree.root->right->package==3){
+      if(tree.root->left->package==10){
+        r.passed=true;
+      }
+    }
   }
 }
 
 int main(){
   auto tester=Test("Binary Tree Tests");
-  tester.add_test("Insert", test_simple_insert_value);
-  tester.add_test("Insert Greater", test_insert_greater);
-  tester.add_test("Insert Lesser", test_insert_lesser);
-  tester.add_test("Insert Many", test_insert_many);
-  tester.add_test("Binary Search", [](test_result &r)->void{
-    auto test_tree=BinaryTree(5);
-    int i=0;
-    while(i<11){
-      if(i!=5){
-        test_tree.insert(i);
-      }
-      i++;
-    }
-    if(test_tree.root->recursive_search("011")==2){
-      r.passed=true;
-    } else {
-      r.passed=false;
-      r.log="Should have gotten 2 got: "+std::to_string(
-        test_tree.root->recursive_search("011")
-      );
-    }
-  });
-  tester.add_test("Binary recursive search", [](test_result &r)->void{
-    auto test_tree=BinaryTree(10);
-    test_tree.insert(5);
-    test_tree.insert(2);
-    test_tree.insert(6);
-    test_tree.insert(20);
-    test_tree.insert(15);
-    if(test_tree.search("0")==5){
-      if(test_tree.search("01")==6){
-        if(test_tree.search("10")==15){
-          r.passed=true;
-        } else {
-          r.passed=false;
-          r.log="Should have gotten 15 got: "+std::to_string(
-            test_tree.search("10")
-          );
-        }
-      } else {
-        r.passed=false;
-        r.log="Should have gotten 6 got: "+std::to_string(
-          test_tree.search("01")
-        );
-      }
-    } else {
-      r.passed=false;
-      r.log="Should have gotten 5 got: "+std::to_string(
-        test_tree.search("0")
-      );
-    }
-  });
-  tester.add_test("Testing binary push", [](test_result &r)->void{
-    auto test_tree=BinaryTree(10);
-    test_tree.push(5, "0");
-    test_tree.push(20, "00");
-    test_tree.push(20, "00");
-    test_tree.push(5, "01");
-    if(test_tree.search("000")!=20){
-      r.passed=false;
-    } else {
-      r.passed=true;
-    }
-  });
+  tester.add_test("Insert Test", insert_test);
+  tester.add_test("Height Test", height_test);
+  tester.add_test("Rotate left Test", rotate_left_test);
+  tester.add_test("Rotate right Test", rotate_right_test);
+  tester.add_test("Left Right Rotate Test", left_right_rotate_test);
+  tester.add_test("Right Left Rotate Test", right_left_rotate_test);
+
+  // auto tree=BinaryTree<int>();
+  // tree.insert(10);
+  // tree.insert(11);
+  // tree.insert(20);
+  // tree.insert(5);
+  // tree.insert(4);
+  // tree.insert(2);
+  // tree.print();
+  // std::cout << std::endl;
+  // tree.root=tree.rotate_left(tree.root);
+  // tree.print();
   tester.run();
   return 0;
 }
